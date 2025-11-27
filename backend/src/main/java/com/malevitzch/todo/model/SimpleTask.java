088c9c6c59b9a8ee65b8382946f10bf4ca9aeb88
malevitzch <malevitzch@gmail.com>
2025-11-27 14:26:39 +0100

    Create a simple basic model and configured the pom.xml to work properly

--- backend/src/main/java/com/malevitzch/todo/model/SimpleTask.java
+++ backend/src/main/java/com/malevitzch/todo/model/SimpleTask.java
@@ -1,5 +1,13 @@
 package com.malevitzch.todo.model;
+import jakarta.persistence.Entity;
 
+@Entity
 public class SimpleTask {
-    
+   private final String name;
+   private boolean completed;
+
+   SimpleTask(String name) {
+      this.name = name;
+      completed = false;
+   }
 }
