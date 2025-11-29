53edc98f9cc1af821694cf10dd41c00401841af2
malevitzch <malevitzch@gmail.com>
2025-11-29 10:13:05 +0100

    A bit of refactoring, added a repository as well

--- backend/src/main/java/com/malevitzch/todo/controller/WebController.java
+++ backend/src/main/java/com/malevitzch/todo/controller/WebController.java
@@ -1,6 +1,7 @@
-package com.malevitzch.todo;
+package com.malevitzch.todo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class WebController {
     @GetMapping("/")
     public String index() {
         return "<h1>Hello world</h1>";
+    }
+    @PostMapping("/task/add/{name}")
+    public boolean addTask(String name) {
+
+        return true;
     }
 }
--- backend/src/main/java/com/malevitzch/todo/model/SimpleTask.java
+++ backend/src/main/java/com/malevitzch/todo/model/SimpleTask.java
@@ -1,19 +1,51 @@
 package com.malevitzch.todo.model;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 
 @Entity
+// We have an index on the name columns since we are likely to search by name
+@Table(indexes = {@Index(name = "idx_name", columnList = "name")})
 public class SimpleTask {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private final String name;
-    private boolean completed;
+    // TODO: use UUIDs for user-visible identifiers
+
+    @Column(nullable = false)
+    private String name;
+    
+    private boolean completed = false;
 
     SimpleTask(String name) {
       this.name = name;
-      completed = false;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    
+    // The JPA (Java Persistence API) requires a default constructor
+    protected SimpleTask() {}
+    
+    public String getTag() {
+        // The tags are name#<id in base36>, used for distinguishing tasks for users
+        return name + "#" + Long.toString(id, 36);
     }
 }
