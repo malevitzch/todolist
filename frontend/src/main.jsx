e1ec1fd5053b846f4a65ddae6848055a67007b85
malevitzch <malevitzch@gmail.com>
2025-12-03 10:47:54 +0100

    Made the react part of the project actually work, added a docker compose which currently only serves the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; // optional, include if you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
 );
