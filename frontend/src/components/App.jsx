b8efdf327b426ba4c9c8dddbdf2548c3a0d095ea
malevitzch <malevitzch@gmail.com>
2025-12-06 15:53:53 +0100

    Added a navbar and some styling

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,9 @@
-function App() {
+import Navbar from "./Navbar.jsx";
+export default function App() {
     return (
-        <div className="bg-blue-500">
+        <div className="bg-blue-300 w-full h-screen">
+            <Navbar />
             <h1>To-Do List!</h1>
         </div>
     )
 }
-
-export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-blue-500 h-[6%]">
+            <h1 className="text-center"> Navbar</h1>
+        </nav>
+    )
+}
