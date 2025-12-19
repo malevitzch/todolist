9c1706abf6c100da71c619c7e6b6d6b4b71be2a2
malevitzch <malevitzch@gmail.com>
2025-12-19 18:23:47 +0100

    Migrated from tailwind 3 to tailwind 4

--- frontend/src/components/tasks/TaskList.jsx
+++ frontend/src/components/tasks/TaskList.jsx
@@ -3,7 +3,10 @@ import TaskBox from "./TaskBox.jsx";
 export default function TaskList() {
     const {data, isLoading, error} = useSimpleTasks();
     // TODO: error
-    if (!isLoading) {
+    if(error) {
+        return <div>Error: {error.message}</div>;
+    }
+    else if(!isLoading) {
         return (
             // TODO: this should be based on task type
             <div className="flex-1 overflow-y-auto">
